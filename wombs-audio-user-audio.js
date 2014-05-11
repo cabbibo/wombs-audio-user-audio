deffa8dbf12959e7adeb048d124005a00c9e2c02
Isaac Cohen <icohen@leapmotion.com>
2014-05-11 15:33:00 -0700

    making it work

--- wombs-audio-user-audio.js
+++ wombs-audio-user-audio.js
@@ -1,58 +1,89 @@
 
 
-  var _ = require( 'underscore' ); 
-
-  var Texture = require( 'wombs-audio-texture' );
-  var Component = require( 'wombs-component' );
+  var _         = require( 'underscore'             ); 
+ 
+  var Component = require( 'wombs-audio-component'  );
+  var Texture   = require( 'wombs-audio-texture'    );
   
-  UserAudio.prototype = new Component();
-  
-  function UserAudio( controller ){
+  var mutate = {};
 
-    this._init();
-
-    
-    this.detect();
-    
-    this.controller = controller;
-    
-
-    var constraints = { audio:true }
-    
-    this.getUserMedia( 
-      constraints , 
-      this.successCallback.bind( this ) , 
-      this.errorCallback.bind( this )
-    );
-
-
-
-  }
-
-  UserAudio.prototype.detect = function(){
-
+  mutate.detect = function(){
       
-    try(){
-      this.getUserMedia =  
+      navigator.getUserMedia =  
         navigator.getUserMedia        || 
         navigator.webkitGetUserMedia  || 
         navigator.mozGetUserMedia;
-    }catch( e){
-      this.onError( 'No User Media' ); 
-    }
 
+      if( !navigator.getUserMedia ){
+        this.onError( 'NO USER MEDIA' );
+      }
 
   }
 
-  UserAudio.prototype.beginLoad = function(){}
-  UserAudio.prototype.endLoad = function(){}
+  mutate.beginLoad = function(){}
+  mutate.endLoad = function(){}
 
-  UserAudio.
+  mutate.successCallback = function( stream ){
+   
+    console.log( 'stream succeed' );
+    this.source = this.ctx.createMediaStreamSource( stream );
+    this.import( this.source );
+
+    console.log( this.ctx );
+
+  }
+  
+  mutate.errorCallback = function(){
+    this.onError( 'Stream Failed' );
+  }
+
 
   UserAudio.prototype.onError = function( string ){
 
     console.log( string );
 
   }
-  
+
+
+  UserAudio.prototype = _.extend( 
+    Component.prototype,
+    mutate
+  );
+
+  function UserAudio( controller , params ){
+
+    Component.call( this , controller );
+
+    this.params = _.defaults( params || {} , {
+
+      fbc:      2048,
+      texture: true,
+      analyser: true,
+
+    });
+
+    this.detect();    
+
+    var constraints = { audio:true }
+    
+    navigator.getUserMedia( 
+      constraints , 
+      this.successCallback.bind( this ) , 
+      this.errorCallback.bind( this )
+    );
+
+
+    if( this.params.analyser || this.params.analyser ){
+      this.createAnalyser( this.params.fbc );
+    }
+
+    if( this.params.texture ){
+      this.createTexture( this.analyser );
+    }
+
+  }
+
+
+
+
   module.exports = UserAudio;
